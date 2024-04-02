ebbe9f84106905ea56270392a755e5ff7e504531
SantosIvanAlvaChavez <ivanals8998@hotmail.com>
2024-04-03 01:34:54 +0200

    add action & HTTP GET method in index.html

---
+++ exercises/02-04-2024/actions.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php 
+        $name = $_GET['name'];
+        $surname = $_GET['surname'];
+    ?>
+
+    <h2>Welcome <?php echo $name; ?> <?php echo $surname; ?></h2>
+</body>
+</html>
