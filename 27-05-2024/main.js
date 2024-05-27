5e64968361e1da6e5f8d88e7dabf92635ab5cc8f
SantosIvanAlvaChavez <ivanals8998@hotmail.com>
2024-05-27 23:51:48 +0200

    add loop do-while

--- 27-05-2024/main.js
+++ 27-05-2024/main.js
@@ -1,2 +1,7 @@
-let word = prompt("Inserisci una parola");
-console.log(word.length);
+let word = prompt("Inserisci una parola che contenga minimo 5 caratteri ").trim();
+
+do {
+    word = prompt("Riprova Inserendo una parola che contenga minimo 5 caratteri").trim()
+} while (word.length < 5);
+
+console.log(word);
