7271bf841d1513a1a0849d30076811d6512229e3
SantosIvanAlvaChavez <ivanals8998@hotmail.com>
2024-05-27 17:45:05 +0200

    loop for

--- 27-05-2024/main.js
+++ 27-05-2024/main.js
@@ -1,20 +1,3 @@
-let number = 8;
-
-{
-    let number = 9;
-    console.log(number);
-    //  Questa variabile locale number esiste solo all'interno di questo blocco di codice e quindi posso dichiararla e assegnarle il valore 9
+for(let i = 0; i <= 20; i+=2){
+    console.log(i);
 }
-
-console.log(number);
-
-let outside = 1;
-
-{
-    let inside = 2;
-    console.log(inside);
-    console.log(outside);
-}
-
-console.log(inside); // Mostrerà un ReferenceError in consola in quanto la variable inside è solo visibile all'interno del blocco di codice.
-console.log(outside);
