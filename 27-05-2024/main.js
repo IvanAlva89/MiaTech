c2d9e349cadb02b2eaedcfe14dee4eb8d59fc1c9
SantosIvanAlvaChavez <ivanals8998@hotmail.com>
2024-05-28 01:08:21 +0200

    add switch statement

--- 27-05-2024/main.js
+++ 27-05-2024/main.js
@@ -4,7 +4,24 @@ while( isNaN(score) || score < 0 || score > 100 ) {
     score = parseInt(prompt("Riprova inserendo un numero tra 0 e 100"));
 }
 
-if( score >= 90 && score <= 100 ) console.log(`Il tuo voto ${score} è ottimo`);
-else if ( score >= 70 && score <= 89 ) console.log(`Il tuo voto ${score} è buono`);
-else if ( score >= 60 && score <= 69 ) console.log(`Il tuo voto ${score} è sufficiente`);
-else console.log(`Il tuo voto ${score} è insufficiente`);
+switch ( typeof score === "number" ) {
+    case ( score >= 90 && score <= 100 ):
+        console.log(`Il tuo voto ${score} è ottimo`)
+        break;
+
+    case ( score >= 70 && score <= 89 ):
+        console.log(`Il tuo voto ${score} è buono`);
+        break;
+
+    case ( score >= 60 && score <= 69 ):
+        console.log(`Il tuo voto ${score} è sufficiente`);
+        break;
+        
+    default:
+        console.log(`Il tuo voto ${score} è insufficiente`);
+}
+
+// if( score >= 90 && score <= 100 ) console.log(`Il tuo voto ${score} è ottimo`);
+// else if ( score >= 70 && score <= 89 ) console.log(`Il tuo voto ${score} è buono`);
+// else if ( score >= 60 && score <= 69 ) console.log(`Il tuo voto ${score} è sufficiente`);
+// else console.log(`Il tuo voto ${score} è insufficiente`);
