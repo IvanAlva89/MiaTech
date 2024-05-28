0c390189f3d5fa5f01b122d30dcf9b4e166ef457
SantosIvanAlvaChavez <ivanals8998@hotmail.com>
2024-05-28 16:55:36 +0200

    reduce & sort

--- 27-05-2024/main.js
+++ 27-05-2024/main.js
@@ -1,14 +1,9 @@
-const students = [
-    { name: 'Mario', grade: 60 },
-    { name: 'Stefania', grade: 30 },
-    { name: 'Lorenzo', grade: 85 },
-    { name: 'Eleonora', grade: 90 },
-    { name: 'Andrea', grade: 59 },
-    { name: 'Elisabetta', grade: 72 },
-];
+const expenses = [10, 28, 70, 30, 45, 100, 63];
 
-const passedStudent = students.filter( student => student.grade >= 60 );
-console.log(passedStudent);
+const sumExpenses = expenses.reduce( (acc, cur) => acc + cur, 0 );
 
-const failedStudent = students.find( student => student.grade < 60 );
-console.log(failedStudent);
+console.log(sumExpenses);
+
+const words = ["banana", "apple", "cherry", "date"];
+
+console.log(words.sort());
