8dd327c5de7d06eb184d7665d5d96f3e23245328
SantosIvanAlvaChavez <ivanals8998@hotmail.com>
2024-05-28 02:10:29 +0200

    create an object

--- 27-05-2024/main.js
+++ 27-05-2024/main.js
@@ -1,17 +1,7 @@
-const words = [];
-
-for( let i = 0; i < 5; i++ ) {
-    let promptWords = prompt("Inserisci una parola").trim();
-
-    while( promptWords === "" || !isNaN(promptWords) ) {
-        promptWords = prompt("Riprova inserendo una parola").trim();
-    }
-
-    words.push(promptWords);
+const person = {
+    name: "John",
+    age: 21,
+    city: "Milan"
 }
 
-console.log(words);
-
-for( let i = 0; i < words.length; i++ ) {
-    if( words[i].length % 2 !== 0 ) console.log(words[i]);
-}
+console.log(person);
