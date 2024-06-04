3f8e8d584bab50826004fb6083b1b0ba8d17ce1f
SantosIvanAlvaChavez <ivanals8998@hotmail.com>
2024-06-04 16:40:18 +0200

    sotto classe elettrica

--- 04-06-2024/main.js
+++ 04-06-2024/main.js
@@ -23,3 +23,22 @@ class Automobile {
         return this._chilometraggio;
     }
 }
+
+class Elettrica extends Automobile {
+    _autonomia = 0;
+
+    constructor(marca, modello, anno, chilometraggio, autonomia) {
+        super(marca, modello, anno, chilometraggio,)
+        this._autonomia = autonomia;
+    }
+
+    descrizione() {
+        return `L'auto ${this._marca}, modello ${this._modello} entrò in vendita nell'anno ${this._anno} con un'autonomia di ${this._autonomia}km`;
+    }
+
+    ricarica(km) {
+        return this._autonomia += km;
+    }
+}
+
+
