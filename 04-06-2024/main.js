c3fba719df75071dfe2f483a8809732c3811fe99
SantosIvanAlvaChavez <ivanals8998@hotmail.com>
2024-06-04 16:19:00 +0200

    metodi & proprieta

--- 04-06-2024/main.js
+++ 04-06-2024/main.js
@@ -2,14 +2,24 @@ class Automobile {
     _marca;
     _modello;
     _anno;
+    _chilometraggio = 0;
 
-    constructor(marca, modello, anno) {
+    constructor(marca, modello, anno, chilometraggio) {
         this._marca = marca;
         this._modello = modello;
         this._anno = anno;
+        this._chilometraggio = chilometraggio;
     }
 
     descrizione() {
         return `L'auto ${this._marca}, modello ${this._modello} entrò in vendita nell'anno ${this._anno}`;
+    }
+
+    aggiungiChilometri(km) {
+        return this._chilometraggio += km;
+    }
+
+    mostraChilometraggio() {
+        return this._chilometraggio;
     }
 }
