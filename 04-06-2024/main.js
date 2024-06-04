b46428fed7741963a5299f4082a7582724ece8e2
SantosIvanAlvaChavez <ivanals8998@hotmail.com>
2024-06-05 00:18:07 +0200

    metodo protetto

--- 04-06-2024/main.js
+++ 04-06-2024/main.js
@@ -31,6 +31,16 @@ class Automobile {
     mostraEta() {
         return `L'eta dell'automobile è ${this.#calcolaEta()}`;
     }
+
+    _controllaChilometri() {
+        const limiteChilometraggio = 100000;
+
+        if( this._chilometraggio > limiteChilometraggio ) {
+            return `ATTENZIONE!: Hai superato i ${limiteChilometraggio} km`;
+        } else {
+            return this.mostraChilometraggio();
+        }
+    }
 }
 
 class Elettrica extends Automobile {
@@ -54,15 +64,18 @@ Automobile.prototype.saluta = function() {
     return `Ciao, sono un'auto ${this._marca}, modello ${this._modello}`
 }
 
-// const fiat = new Automobile("Fiat", "Panda", 2030, 0);
+// const fiat = new Automobile("Fiat", "Panda", 2030, 100);
 // console.log(fiat.descrizione());
-// console.log(fiat.aggiungiChilometri(50));
+// console.log(fiat.aggiungiChilometri(5000000));
 // console.log(fiat.saluta());
 // console.log(fiat.mostraEta());
+// console.log(fiat._controllaChilometri());
 
 // const toyota = new Elettrica("Toyota", "Electric", 2040, 10, 500);
 // console.log(toyota.descrizione());
-// console.log(toyota.ricarica(100));
+// console.log(toyota.ricarica(1000));
 // console.log(toyota.saluta());
+// console.log(toyota.aggiungiChilometri(100000));
+// console.log(toyota._controllaChilometri());
 
 
