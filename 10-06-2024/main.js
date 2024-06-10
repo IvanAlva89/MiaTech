e992cb42d026c39f0ad7066c8bcdc53e7457dcd0
SantosIvanAlvaChavez <ivanals8998@hotmail.com>
2024-06-10 23:58:26 +0200

    stringa multilinea con template literals

--- 10-06-2024/main.js
+++ 10-06-2024/main.js
@@ -1,7 +1,13 @@
 const name = "John";
-
 const lastName = "Smith";
+const age = 30;
+const city = "Milan";
 
-const fullName = `${name} ${lastName}`;
+const multiline = `
+    ${name} 
+    ${lastName} 
+    ${age} 
+    ${city}
+`;
 
-console.log(fullName);
+console.log(multiline);
