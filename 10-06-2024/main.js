e489ba8942f23c4bd5724a7688f337e69a17bc1e
SantosIvanAlvaChavez <ivanals8998@hotmail.com>
2024-06-10 16:42:50 +0200

    valori predefiniti

--- 10-06-2024/main.js
+++ 10-06-2024/main.js
@@ -1,6 +1,6 @@
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const numbers = [1, 2];
 
-const [firstNumber, _, thirdNumber] = numbers;
+const [firstNumber, secondNumber, thirdNumber = 3, fourthNumber = 4, fifthNumber = 5] = numbers;
 
-console.log(firstNumber, thirdNumber);
+console.log(firstNumber, thirdNumber, thirdNumber, fourthNumber, fifthNumber);
