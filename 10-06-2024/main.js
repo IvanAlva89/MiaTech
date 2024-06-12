bab90cf200844c9555e263561bd5a14145a19da5
SantosIvanAlvaChavez <ivanals8998@hotmail.com>
2024-06-12 10:42:36 +0200

    fix: valori predefiniti

--- 10-06-2024/main.js
+++ 10-06-2024/main.js
@@ -1,7 +1,14 @@
-const message = () => console.log("This message is printed every 1 second");
+// const message = () => console.log("This message is printed every 1 second");
 
-const clearMessage = setInterval(message, 1000);
+// const clearMessage = setInterval(message, 1000);
 
-setTimeout(() => {
-    clearInterval(clearMessage)
-}, 5000);
+// setTimeout(() => {
+//     clearInterval(clearMessage)
+// }, 5000);
+
+
+const numbers = [1, 2];
+
+const [firstNumber, secondNumber, thirdNumber = 3, fourthNumber = 4, fifthNumber = 5] = numbers;
+
+console.log(firstNumber, secondNumber, thirdNumber, fourthNumber, fifthNumber);
