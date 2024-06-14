a5dc6a88cb815e73364a9b0e66b023bee729fa20
SantosIvanAlvaChavez <ivanals8998@hotmail.com>
2024-06-14 16:15:09 +0200

    finally

--- 10-06-2024/main.js
+++ 10-06-2024/main.js
@@ -1,3 +1,16 @@
+const checkParam = (param) => {
+    try {
+        const result = param.toUpperCase();
+        console.log(result);
+    } catch (err) {
+        console.error("Si è verificato un problema", err.message)
+    } finally {
+        console.log("Operazione completata")
+    }
+}
+
+checkParam();
+
 
 // const checkVariable = (variable) => {
 //     if(variable === 0) {
@@ -11,9 +24,6 @@
 
 // checkVariable(1);
 
-
-
-
 // try {
 //     let result = 10 / 0;
         
@@ -26,23 +36,23 @@
 //     console.error("Errore: " , error.message);
 // }
 
-const checkString = (string) => {
-    try {
-       const str = Number(string);
-       if(isNaN(str)) {
-            throw new TypeError("Il valore inserito non e un numero valido")
-       }
-       if(str === 0) {
-        throw new Error("Il valore non puo essere 0")
-       }
-    return str;
-    } catch (error) {
-        if(error instanceof TypeError) {
-            console.error("Errore di tipo :" , error.message)
-        } else {
-            console.error("Errore generico :", error.message)
-        }
-    } 
-}
+// const checkString = (string) => {
+//     try {
+//        const str = Number(string);
+//        if(isNaN(str)) {
+//             throw new TypeError("Il valore inserito non e un numero valido")
+//        }
+//        if(str === 0) {
+//         throw new Error("Il valore non puo essere 0")
+//        }
+//     return str;
+//     } catch (error) {
+//         if(error instanceof TypeError) {
+//             console.error("Errore di tipo :" , error.message)
+//         } else {
+//             console.error("Errore generico :", error.message)
+//         }
+//     } 
+// }
 
-checkString("123");
+// checkString("123");
