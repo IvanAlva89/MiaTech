740f5f75dc30bbe4bc4ced53c680e7505f7c3ad5
SantosIvanAlvaChavez <ivanals8998@hotmail.com>
2024-06-14 15:59:10 +0200

    fix console.error & console.warn

--- 10-06-2024/main.js
+++ 10-06-2024/main.js
@@ -1,11 +1,38 @@
-try {
-    let result = 10 / 0;
-        
-    if(result === Infinity) {
-        throw new Error("Il divisore non puo essere uguale a 0");
+
+const checkVariable = (variable) => {
+    if(variable === 0) {
+        console.error("La variable non puo essere 0")
+    } else if(variable === 1) {
+        console.warn("Avviso: La variabile ha valore 1")
+    } else {
+        console.log("Variabile corretta")
     }
-    
-    console.log(result);
-} catch (error) {
-    console.error("Errore: " , error.message);
 }
+
+checkVariable(1);
+
+
+
+
+// try {
+//     let result = 10 / 0;
+        
+//     if(result === Infinity) {
+//         throw new Error("Il divisore non puo essere uguale a 0");
+//     }
+
+//     console.log(result);
+// } catch (error) {
+//     console.error("Errore: " , error.message);
+// }
+
+// try {
+//     let string = "ciao";
+//     let result = string.map(word => word.length)
+//     if(!result) throw new SyntaxError("Non e un array")
+//     console.log(result);
+//     // else throw new Error("errore 2")
+// } catch (error) {
+//     if(error instanceof SyntaxError) console.log(error);
+
+// } 
