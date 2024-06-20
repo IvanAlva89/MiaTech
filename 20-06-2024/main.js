5016dd0a8073a61a8493e91ced3ba85745088500
SantosIvanAlvaChavez <ivanals8998@hotmail.com>
2024-06-20 18:01:14 +0200

    gestione degli errori in una catena di promesse

--- 20-06-2024/main.js
+++ 20-06-2024/main.js
@@ -1,10 +1,18 @@
-const promise = (boolean) => {
+const promise = () => {
     return new Promise((resolve, reject) => {
-        if(typeof boolean === "boolean") resolve(boolean)
-        reject(new Error(`${boolean} non è un valore booleano`))
-    })  
+        let number = Math.floor(Math.random() * 10);
+        if(number >= 5) {
+            resolve(number);
+        } else {
+            reject(`Il numero: ${number} è minore di 5`);
+        }
+    })
 }
 
-promise("ciao")
-    .then(result => console.log(`Il parametro ${result} è un valore booleano`))
-    .catch(error => console.error(error.message))
+promise()
+    .then(result => result * 2)
+    .then(finalResult => {
+        console.log(finalResult);
+        throw new Error("Errore durante l'elaborazione del risultato finale");
+    })
+    .catch(error => console.error(error))
