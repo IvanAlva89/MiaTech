287b8e9e5c74f6998e5a5035952ea8d5a076a837
SantosIvanAlvaChavez <ivanals8998@hotmail.com>
2024-06-20 23:32:48 +0200

    Promise all

--- 20-06-2024/main.js
+++ 20-06-2024/main.js
@@ -1,18 +1,22 @@
-const promise = () => {
-    return new Promise((resolve, reject) => {
-        let number = Math.floor(Math.random() * 10);
-        if(number >= 5) {
-            resolve(number);
-        } else {
-            reject(`Il numero: ${number} è minore di 5`);
-        }
+const firstPromise = () => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Prima promessa risolta")
+        }, 2000)
     })
 }
 
-promise()
-    .then(result => result * 2)
-    .then(finalResult => {
-        console.log(finalResult);
-        throw new Error("Errore durante l'elaborazione del risultato finale");
+const secondPromise = () => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Seconda promessa risolta")
+        }, 5000)
     })
-    .catch(error => console.error(error))
+}
+
+Promise.all([firstPromise(), secondPromise()])
+    .then(values => {
+        console.log(values[0])
+        console.log(values[1])
+    })
+    
