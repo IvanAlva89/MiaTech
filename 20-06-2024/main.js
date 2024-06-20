959c97c80a9f030d70fbd64435d6f3c3ebf7dc36
SantosIvanAlvaChavez <ivanals8998@hotmail.com>
2024-06-21 01:40:51 +0200

    Richiesta post

--- 20-06-2024/main.js
+++ 20-06-2024/main.js
@@ -1,8 +1,18 @@
-const API_KEY = "https://jsonplaceholder.typicode.com/users";
+const API_KEY = "https://jsonplaceholder.typicode.com/posts";
 
-const fetchData = async () => {
+const postData = async () => {
     try {
-        const response = await fetch(API_KEY);
+        const response = await fetch(API_KEY, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                title: "New title",
+                body: "Lorem ipsum lorem ipsum lorem ipsum",
+                userId: 1
+            })
+        });
         if(!response.ok) throw new Error("Si e verificato un errore durante la richiesta");
         const data = await response.json();
         console.log(data);
@@ -11,4 +21,4 @@ const fetchData = async () => {
     }
 }
 
-fetchData();
+postData();
