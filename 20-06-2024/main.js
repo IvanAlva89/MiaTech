dfaa2f1589c51c082c58a14ec3bd5db325358d8f
SantosIvanAlvaChavez <ivanals8998@hotmail.com>
2024-06-20 15:55:15 +0200

    promessa con finally

--- 20-06-2024/main.js
+++ 20-06-2024/main.js
@@ -1,15 +1,16 @@
 const promise = () => {
     return new Promise((resolve, reject) => {
-        if(!true) {
+        if(true) {
             setTimeout( () => {
                 resolve("Promessa risolta dopo 2 secondi");
             }, 2000)
         } else {
-            reject("La condizione non è soddisfatta ")
+            reject("La condizione non è soddisfatta ");
         }
     })
 }
 
 promise()
     .then(message => console.log(message))
-    .catch(error => console.log(error));
+    .catch(error => console.log(error))
+    .finally(() => console.log("Azione completata indipendentemente dall'esito della promessa"));
