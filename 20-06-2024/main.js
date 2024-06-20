f56e333ef96a129cafad874e07feb38ec7f2ff83
SantosIvanAlvaChavez <ivanals8998@hotmail.com>
2024-06-20 17:36:27 +0200

    gestione degli errori con catch

--- 20-06-2024/main.js
+++ 20-06-2024/main.js
@@ -1,18 +1,10 @@
 const promise = () => {
     return new Promise((resolve, reject) => {
-        let number = Math.floor(Math.random() * 10);
-        if(number >= 5) {
-            resolve(number);
-        } else {
-            reject(`Il numero: ${number} è minore di 5`);
-        }
+        const messageError = "Si è verificato un errore";
+        reject(messageError);
     })  
 }
 
 promise()
-    .then(num => {
-        console.log(num)
-        return num * 3;
-    })
     .then(result => console.log(result))
     .catch(error => console.error(error))
