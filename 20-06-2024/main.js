9049d4e7dda1cf9147cf4541629dfec3d3150528
SantosIvanAlvaChavez <ivanals8998@hotmail.com>
2024-06-20 17:45:59 +0200

    gestione degli errori con then e catch

--- 20-06-2024/main.js
+++ 20-06-2024/main.js
@@ -1,10 +1,10 @@
-const promise = () => {
+const promise = (boolean) => {
     return new Promise((resolve, reject) => {
-        const messageError = "Si è verificato un errore";
-        reject(messageError);
+        if(typeof boolean === "boolean") resolve(boolean)
+        reject(new Error(`${boolean} non è un valore booleano`))
     })  
 }
 
-promise()
-    .then(result => console.log(result))
-    .catch(error => console.error(error))
+promise("ciao")
+    .then(result => console.log(`Il parametro ${result} è un valore booleano`))
+    .catch(error => console.error(error.message))
