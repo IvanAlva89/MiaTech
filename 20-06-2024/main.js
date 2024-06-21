4d036639cdee632d30ec5201c968019d15ebc87f
SantosIvanAlvaChavez <ivanals8998@hotmail.com>
2024-06-21 16:01:11 +0200

    callback annidati

--- 20-06-2024/main.js
+++ 20-06-2024/main.js
@@ -1,14 +1,43 @@
-const API_KEY = "https://jsonplaceholder.typicode.com/posts";
-
-const fetchData = async () => {
-    try {
-        const response = await fetch(API_KEY);
-        if(!response.ok) throw new Error("Si e verificato un errore durante la richiesta");
-        const data = await response.json();
-        console.log(data);
-    } catch (error) {
-        console.error("Errore: ", error.message);
-    }
+const firstFn = (callback) => {
+    setTimeout(() => {
+        console.log("Prima operazione eseguita");
+        callback("Risultato prima funzione");
+    }, 2000)
 }
 
-fetchData();
+const secondFn = (firstResult, callback) => {
+    setTimeout(() => {
+        console.log("Seconda operazione eseguita: " , firstResult);
+        callback("Risultato seconda funzione")  
+    }, 3000)
+}
+
+const thirdFn = () => {
+    firstFn((firstResult) => {
+        secondFn(firstResult, (secondResult) => {
+            console.log("Operazioni completate" , secondResult)
+        })
+    });
+}
+
+thirdFn();
+
+
+
+
+
+
+// const API_KEY = "https://jsonplaceholder.typicode.com/posts";
+
+// const fetchData = async () => {
+//     try {
+//         const response = await fetch(API_KEY);
+//         if(!response.ok) throw new Error("Si e verificato un errore durante la richiesta");
+//         const data = await response.json();
+//         console.log(data);
+//     } catch (error) {
+//         console.error("Errore: ", error.message);
+//     }
+// }
+
+// fetchData();
