c7a27cbd4161a6c7c609bbd3adae6a17e808268b
SantosIvanAlvaChavez <ivanals8998@hotmail.com>
2024-06-28 15:56:55 +0200

    fix: catch added

--- 20-06-2024/main.js
+++ 20-06-2024/main.js
@@ -1,18 +1,8 @@
 const API_KEY = "https://jsonplaceholder.typicode.com/posts";
 
-const postData = async () => {
+const fetchData = async () => {
     try {
-        const response = await fetch(API_KEY, {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                title: "New title",
-                body: "Lorem ipsum lorem ipsum lorem ipsum",
-                userId: 1
-            })
-        });
+        const response = await fetch(API_KEY);
         if(!response.ok) throw new Error("Si e verificato un errore durante la richiesta");
         const data = await response.json();
         console.log(data);
@@ -21,7 +11,32 @@ const postData = async () => {
     }
 }
 
-postData();
+fetchData();
+
+// const API_KEY = "https://jsonplaceholder.typicode.com/posts";
+
+// const postData = async () => {
+//     try {
+//         const response = await fetch(API_KEY, {
+//             method: "POST",
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             },
+//             body: JSON.stringify({
+//                 title: "New title",
+//                 body: "Lorem ipsum lorem ipsum lorem ipsum",
+//                 userId: 1
+//             })
+//         });
+//         if(!response.ok) throw new Error("Si e verificato un errore durante la richiesta");
+//         const data = await response.json();
+//         console.log(data);
+//     } catch(error) {
+//         console.error(error.message)
+//     }
+// }
+
+// postData();
 
 // const firstFn = (callback) => {
 //     setTimeout(() => {
