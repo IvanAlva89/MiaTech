70dea57c49e81b157a10c365129a7daaf8c871c5
SantosIvanAlvaChavez <ivanals8998@hotmail.com>
2024-06-28 15:52:54 +0200

    fix: catch added

--- 20-06-2024/main.js
+++ 20-06-2024/main.js
@@ -1,26 +1,51 @@
-const firstFn = (callback) => {
-    setTimeout(() => {
-        console.log("Prima operazione eseguita");
-        callback("Risultato prima funzione");
-    }, 2000)
+const API_KEY = "https://jsonplaceholder.typicode.com/posts";
+
+const postData = async () => {
+    try {
+        const response = await fetch(API_KEY, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                title: "New title",
+                body: "Lorem ipsum lorem ipsum lorem ipsum",
+                userId: 1
+            })
+        });
+        if(!response.ok) throw new Error("Si e verificato un errore durante la richiesta");
+        const data = await response.json();
+        console.log(data);
+    } catch(error) {
+        console.error(error.message)
+    }
 }
 
-const secondFn = (firstResult, callback) => {
-    setTimeout(() => {
-        console.log("Seconda operazione eseguita: " , firstResult);
-        callback("Risultato seconda funzione")  
-    }, 3000)
-}
+postData();
 
-const thirdFn = () => {
-    firstFn((firstResult) => {
-        secondFn(firstResult, (secondResult) => {
-            console.log("Operazioni completate" , secondResult)
-        })
-    });
-}
+// const firstFn = (callback) => {
+//     setTimeout(() => {
+//         console.log("Prima operazione eseguita");
+//         callback("Risultato prima funzione");
+//     }, 2000)
+// }
 
-thirdFn();
+// const secondFn = (firstResult, callback) => {
+//     setTimeout(() => {
+//         console.log("Seconda operazione eseguita: " , firstResult);
+//         callback("Risultato seconda funzione")  
+//     }, 3000)
+// }
+
+// const thirdFn = () => {
+//     firstFn((firstResult) => {
+//         secondFn(firstResult, (secondResult) => {
+//             console.log("Operazioni completate" , secondResult)
+//         })
+//     });
+// }
+
+// thirdFn();
 
 
 
