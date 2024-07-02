f28b0d606e2ef231112dc011173ad08c5b7f038e
SantosIvanAlvaChavez <ivanals8998@hotmail.com>
2024-07-02 16:23:23 +0200

    oggetto document

--- 02-07-2024/main.js
+++ 02-07-2024/main.js
@@ -1,8 +1,1 @@
-sessionStorage.setItem("number", 5);
-console.log(`Valore salvato nel sessionStorage: ${sessionStorage.number}`);
-
-sessionStorage.getItem("number");
-console.log(`Valore recuperato dal sessionStorage: ${sessionStorage.number}`);
-
-sessionStorage.removeItem("number");
-console.log(`Valore rimosso dal sessionStorage: ${sessionStorage.number}`);
+console.log(document);
