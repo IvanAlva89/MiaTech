ea18b8178ed760134f4c641d67d310fda7d81710
SantosIvanAlvaChavez <ivanals8998@hotmail.com>
2024-07-07 12:51:09 +0200

    stili hover

--- 03-07-2024/main.js
+++
@@ -1,8 +0,0 @@
-const darkModeToggle = document.getElementById("darkModeToggle");
-const darkModeStatus = document.getElementById("darkModeStatus");
-
-darkModeToggle.addEventListener("click", () => {
-    document.body.classList.toggle("dark");
-
-    darkModeStatus.innerHTML = document.body.classList.contains("dark") ? "Disattiva dark mode" : "Attiva dark mode";
-});
