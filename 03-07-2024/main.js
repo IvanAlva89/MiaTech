d76520d813f433b118ac2cdcf8c7467bc95d3b0b
SantosIvanAlvaChavez <ivanals8998@hotmail.com>
2024-07-07 11:45:01 +0200

    add main.js

---
+++ 03-07-2024/main.js
@@ -0,0 +1,8 @@
+const darkModeToggle = document.getElementById("darkModeToggle");
+const darkModeStatus = document.getElementById("darkModeStatus");
+
+darkModeToggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+
+    darkModeStatus.innerHTML = document.body.classList.contains("dark") ? "Disattiva dark mode" : "Attiva dark mode";
+})
