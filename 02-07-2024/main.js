b58f957817cbc472566884e110a3501093af7ae9
SantosIvanAlvaChavez <ivanals8998@hotmail.com>
2024-07-10 10:42:51 +0200

    fixed: js file

--- 02-07-2024/main.js
+++ 02-07-2024/main.js
@@ -1,31 +1,31 @@
-const createCookie = (key, value) => {
-    localStorage.setItem(key, value);
-    console.log("Cookie salvato nel localStorage");
-    sessionStorage.setItem(key, value);
-    console.log("Cookie salvato nel sessionStorage");
-}
-const getCookie = () => {
-    const localValue = localStorage.getItem("miaAcademy");
-    console.log(localValue);
-    const sessionValue = sessionStorage.getItem("miaAcademy");
-    console.log(sessionValue);
-}
+// const createCookie = (key, value) => {
+//     localStorage.setItem(key, value);
+//     console.log("Cookie salvato nel localStorage");
+//     sessionStorage.setItem(key, value);
+//     console.log("Cookie salvato nel sessionStorage");
+// }
+// const getCookie = () => {
+//     const localValue = localStorage.getItem("miaAcademy");
+//     console.log(localValue);
+//     const sessionValue = sessionStorage.getItem("miaAcademy");
+//     console.log(sessionValue);
+// }
 
-const removeCookie = () => {
-    localStorage.removeItem("miaAcademy");
-    sessionStorage.removeItem("miaAcademy");
-}
+// const removeCookie = () => {
+//     localStorage.removeItem("miaAcademy");
+//     sessionStorage.removeItem("miaAcademy");
+// }
 
-createCookie("miaAcademy", true);
-getCookie();
-removeCookie();
+// createCookie("miaAcademy", true);
+// getCookie();
+// removeCookie();
 
 
-// const form = document.querySelector("#form");
-// const name = document.querySelector("#name");
-// const surname = document.querySelector("#surname");
+const form = document.querySelector("#form");
+const name = document.querySelector("#name");
+const surname = document.querySelector("#surname");
 
-// form.addEventListener("submit", (e) => {
-//     if(!name.value || !surname.value) alert("i campi non sono compilati");
-//     e.preventDefault();
-// })
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if(!name.value || !surname.value) alert("i campi non sono compilati");
+})
