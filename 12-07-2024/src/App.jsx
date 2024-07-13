8aceab1a0c6ec615ec422c57a1d7e1cc85953303
SantosIvanAlvaChavez <ivanals8998@hotmail.com>
2024-07-13 15:52:04 +0200

    contatore semplice

--- 12-07-2024/src/App.jsx
+++ 12-07-2024/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import HelloWorld from './components/HelloWorld';
+import Counter from './components/Counter';
+// import HelloWorld from './components/HelloWorld';
 
 const App = () => {
 
   return (
     <>
       <h1>Title App</h1>
-      <HelloWorld />
+      {/* <HelloWorld /> */}
+      <Counter />
     </>
   )
 }
---
+++ 12-07-2024/src/components/Counter.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Counter = () => {
+  return (
+    <div>Count: 0</div>
+  )
+}
+
+export default Counter
