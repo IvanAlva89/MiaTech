8884cf3d58feda888a22f4a070be73fdb2248c29
SantosIvanAlvaChavez <ivanals8998@hotmail.com>
2024-07-13 15:58:08 +0200

    incrementa contatore

--- 12-07-2024/src/components/Counter.jsx
+++ 12-07-2024/src/components/Counter.jsx
@@ -1,10 +1,15 @@
 import React, { useState } from 'react'
 
 const Counter = () => {
-    const [counter, setCounter] = useState(0)
+    const [counter, setCounter] = useState(0);
+
+    const handleIncrement = () => setCounter(counter + 1);
 
     return (
-        <div>Count: {counter}</div>
+        <>
+            <div>Count: {counter}</div>
+            <button onClick={handleIncrement}>+1</button>
+        </>
     )
 }
 
