750211a7892535e50a786bcca23c62d322b8e7d5
SantosIvanAlvaChavez <ivanals8998@hotmail.com>
2024-07-13 15:48:22 +0200

    hello world

---
+++ 12-07-2024/src/components/HelloWorld.jsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+class HelloWorld extends Component {
+    render() {
+        return (
+            <p>Helloword paragraph lorem ipsum lorem ipsum</p>
+        )
+    }
+}
+
+export default HelloWorld;
