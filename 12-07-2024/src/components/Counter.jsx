0da4dc994621d6b93ef7659b13f2c401de8424d0
SantosIvanAlvaChavez <ivanals8998@hotmail.com>
2024-07-13 16:08:31 +0200

    reset contatore

--- 12-07-2024/src/components/Counter.jsx
+++ 12-07-2024/src/components/Counter.jsx
@@ -4,12 +4,14 @@ const Counter = () => {
     const [counter, setCounter] = useState(0);
 
     const handleIncrement = () => setCounter(counter + 1);
+    const handleReset = () => setCounter(0);
     const handleDecrement = () => setCounter(value => value - 1);
 
     return (
         <>
             <div>Count: {counter}</div>
             <button onClick={handleIncrement}>+1</button>
+            <button onClick={handleReset}>Reset</button>
             <button onClick={handleDecrement}>-1</button>
         </>
     )
