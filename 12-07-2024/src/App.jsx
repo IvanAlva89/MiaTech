501588847c086503c4997fe1946d3b8865b07d83
SantosIvanAlvaChavez <ivanals8998@hotmail.com>
2024-07-17 01:02:04 +0200

    props children layout

--- 12-07-2024/src/App.jsx
+++ 12-07-2024/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import ItemList from './components/ItemList';
 // import Counter from './components/Counter';
 // import HelloWorld from './components/HelloWorld';
 // import TextInput from './components/TextInput';
 // import LoginForm from './components/LoginForm';
 // import UncontrolledInput from './components/UncontrolledInput';
+// import ItemList from './components/ItemList';
+import Card from './components/Card';
 
 const App = () => {
   const colors = ["Rosso", "Giallo", "Verde", "Bianco", "Nero", "Arancione"];
@@ -17,7 +18,8 @@ const App = () => {
       {/* <TextInput /> */}
       {/* <LoginForm /> */}
       {/* <UncontrolledInput /> */}
-      <ItemList colors={colors}/>
+      {/* <ItemList colors={colors}/> */}
+      <Card />
     </>
   )
 }
