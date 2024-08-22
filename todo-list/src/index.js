058a46567bf6dda0f6900d122f0a6fc822e65968
SantosIvanAlvaChavez <ivanals8998@hotmail.com>
2024-08-22 22:51:23 +0200

    aggiungere una funzione per aggiungere un Todo

--- todo-list/src/index.js
+++ todo-list/src/index.js
@@ -1,3 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var todos = [];
+var addTodo = function (title) {
+    var newTodo = {
+        id: new Date().getTime() + 1,
+        title: title,
+        completed: false
+    };
+    todos.push(newTodo);
+    return newTodo;
+};
+// console.log(addTodo("Imparare React"));
+// console.log(addTodo("Imparare Typescript"));
+// console.log(addTodo("Imparare Javascript"))
+// console.log(todos);
--- todo-list/src/index.ts
+++ todo-list/src/index.ts
@@ -1,3 +1,19 @@
 import { Todo } from './types';
 
 const todos: Todo[] = [];
+
+const addTodo = (title: string) => {
+    const newTodo: Todo = {
+        id: new Date().getTime() + 1,
+        title,
+        completed: false
+    }
+    todos.push(newTodo);
+
+    return newTodo;
+}
+
+// console.log(addTodo("Imparare React"));
+// console.log(addTodo("Imparare Typescript"));
+// console.log(addTodo("Imparare Javascript"))
+// console.log(todos);
